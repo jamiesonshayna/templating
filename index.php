a747663e8fb47acbea2c84df723a8b75a0d40e13
Shayna Jamieson <jamieson.shayna@gmail.com>
2020-01-27 11:30:30 -0800

    Add home screen with arrays

--- index.php
+++ index.php
@@ -22,13 +22,23 @@ $f3 = Base::instance(); // invoke static
 // define a default route
 // when the user navigates to the route directory of the project
 // this is what they should see
-$f3->route('GET /', function() {
-    echo "<h1>Hello there</h1>";
-//    // create a new view object by instantiating the fat-free templating class
-//    $view = new Template();
-//
-//    // on the object template we render the home page through this route
-//    echo $view->render('views/home.html');
+$f3->route('GET /', function($f3) {
+    // -> is like . notation (set a title variable to the string- "All About Templating")
+    $f3->set('title', 'All About Templating');
+    $f3->set('temp', 67);
+    $f3->set('color', 'purple');
+
+    // create a far free array called bookmarks
+    $f3->set('bookmarks', array('http:google.com', 'http:linkedin.com', 'http:gmail.com'));
+
+    // define an associative array of desserts
+    $f3->set('desserts', array('chocolate' => 'Chocolate Mousse', 'vanilla' => 'Vanilla Custard', 'strawberry' => 'Strawberry Shortcake'));
+
+    // create a new view object by instantiating the fat-free templating class
+    $view = new Template();
+
+    // on the object template we render the home page through this route
+    echo $view->render('views/home.html');
 });
 
 
